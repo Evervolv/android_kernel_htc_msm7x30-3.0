cb42b2816556c0d2e687e80a4ba6075488ee36eb
Jay Wu <jaywu.alt@gmail.com>
2012-11-11 20:47:41 +0800

    power: remove HTC blobs and sync-ed from CAF

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -23,7 +23,6 @@
 #include <linux/slab.h>
 #include <linux/suspend.h>
 #include <linux/syscore_ops.h>
-#include <linux/ftrace.h>
 #include <trace/events/power.h>
 
 #include "power.h"
@@ -213,9 +212,7 @@ int suspend_devices_and_enter(suspend_state_t state)
 		if (error)
 			goto Close;
 	}
-	if (!suspend_console_deferred)
-		suspend_console();
-	ftrace_stop();
+	suspend_console();
 	suspend_test_start();
 	error = dpm_suspend_start(PMSG_SUSPEND);
 	if (error) {
@@ -232,9 +229,7 @@ int suspend_devices_and_enter(suspend_state_t state)
 	suspend_test_start();
 	dpm_resume_end(PMSG_RESUME);
 	suspend_test_finish("resume devices");
-	ftrace_start();
-	if (!suspend_console_deferred)
-		resume_console();
+	resume_console();
  Close:
 	if (suspend_ops->end)
 		suspend_ops->end();

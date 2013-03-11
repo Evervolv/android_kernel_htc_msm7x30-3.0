1ed26e86bfbc287af84cf0344cc492a3de17976d
Jay Wu <cn.fyodor@gmail.com>
2013-03-11 22:46:56 +0800

    qdsp5v2: Remove the log spam
    Change-Id: I2092c267795a8a126d722d63a4b059c0b97b9ed5

--- arch/arm/mach-msm/htc_acoustic_7x30.c
+++ arch/arm/mach-msm/htc_acoustic_7x30.c
@@ -57,7 +57,7 @@
 #define ACOUSTIC_ENABLE_BEATS		_IOW(ACOUSTIC_IOCTL_MAGIC, 42, unsigned)
 #define ACOUSTIC_GET_AIC3008_STATE	_IOW(ACOUSTIC_IOCTL_MAGIC, 43, unsigned)
 
-#define D(fmt, args...) printk(KERN_INFO "[AUD] htc-acoustic: "fmt, ##args)
+#define D(fmt, args...) do { } while (0)
 #define E(fmt, args...) printk(KERN_ERR "[AUD] htc-acoustic: "fmt, ##args)
 
 #define SHARE_PAGES 4
